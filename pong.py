071abca9d1cd99ada601c3f1de5dd12527c925f9
honda12poke <s.honda@keio.jp>
2018-11-04 21:15:03 +0900

    gif

--- pong.py
+++ pong.py
@@ -16,19 +16,16 @@ if __name__ == "__main__":
     max_grad_norm = 0.1
     gpu = True
     epsilon = 0.3
-    lr_ = 0.00001 
+    lr = 0.00005
     JST = timezone(timedelta(hours=+9), "JST")
     print(datetime.now(JST))
-    for i in range(1,5):
-        lr = lr_ * i
-        name = "-lr" + str(lr) + "-"
-        ppo = PPO(ENV, max_epochs, N, T, batch_size, lr=lr, v_loss_coef=v_loss_coef, max_grad_norm=max_grad_norm, epsilon=epsilon, gpu=gpu)
-        try:
-            rs = ppo.run(name=name)
-        except KeyboardInterrupt:
-            pass
-        #torch.save(ppo.model.to("cpu").state_dict(), "pong/ppo.pth")
-        print(datetime.now(JST))
-        plt.plot(range(len(rs)), rs)
-        plt.savefig("pong/rs"+name[:-1]+".png")
-        plt.close()
+    name = "-lr" + str(lr) + "-"
+    ppo = PPO(ENV, max_epochs, N, T, batch_size, lr=lr, v_loss_coef=v_loss_coef, max_grad_norm=max_grad_norm, epsilon=epsilon, gpu=gpu)
+    try:
+        rs = ppo.run(name=name)
+    except KeyboardInterrupt:
+        pass
+    print(datetime.now(JST))
+    plt.plot(range(len(rs)), rs)
+    plt.savefig("pong/rs"+name[:-1]+".png")
+    plt.close()
